300fb93482e15635d15e97b4c00b365ab4138ba7
hsiang <1207victory@gmail.com>
2017-03-14 15:14:23 +0800

    add parellel for and private api

--- raytracing.c
+++ raytracing.c
@@ -467,6 +467,8 @@ void raytracing(uint8_t *pixels, color background_color,
     idx_stack stk;
 
     int factor = sqrt(SAMPLES);
+
+    #pragma omp parallel for private( d, stk, object_color)
     for (int j = 0; j < height; j++) {
         for (int i = 0; i < width; i++) {
             double r = 0, g = 0, b = 0;
